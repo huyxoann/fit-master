cfb0cdf107d6fc705d9f3531304d7fd679bbc0fd
huyxoann <huytn.21it@vku.udn.vn>
2024-10-25 11:57:21 +0700

    project: add assets folder

---
+++ lib/src/features/welcome/screens/welcome_page_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
